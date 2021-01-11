852c938364ff314b62d744452e12120cb52f0051
sidak-245 <74825336+sidak-245@users.noreply.github.com>
2021-01-11 19:52:46 +0530

    Add files via upload

---
+++ constraint.js
@@ -0,0 +1,16 @@
+class Chain{
+constructor(body1,body2){
+var options= {
+bodyA:body1,
+bodyB:body2
+
+}
+this.chain= Constraint.create(options)
+World.add(world,this.chain)
+}
+display(){
+    line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+}
+
+   
+}
